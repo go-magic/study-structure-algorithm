c7872df811e2297692f84b6411449c4ed64c392b
lihao <568330385@qq.com>
2021-09-24 19:00:13 +0800

    编写完成规则

--- structure/tree/node/redBlackNode.go
+++ structure/tree/node/redBlackNode.go
@@ -36,11 +36,6 @@ func (r *RedBlackNode) Insert(data int) *RedBlackNode {
 	if data < r.Data {
 		if r.Left == nil {
 			r.Left = NewRedBlackNode(data)
-			//把父节点变为黑色统一处理
-			if r.Color == Red {
-				r.setBlackColor()
-			}
-			//父节点是黑色的则当前节点是平衡的
 			return r
 		}
 		r.Left = r.Left.Insert(data)
@@ -48,11 +43,6 @@ func (r *RedBlackNode) Insert(data int) *RedBlackNode {
 	if data > r.Data {
 		if r.Right == nil {
 			r.Right = NewRedBlackNode(data)
-			//把父节点变为黑色统一处理
-			if r.Color == Red {
-				r.setBlackColor()
-			}
-			//父节点是黑色的则当前节点是平衡的
 			return r
 		}
 		r.Right = r.Right.Insert(data)
@@ -64,46 +54,197 @@ func (r *RedBlackNode) Insert(data int) *RedBlackNode {
 设置节点颜色并改变黑色节点的总长度
 */
 func (r *RedBlackNode) setBlackColor() {
+	if r.Color == Red {
+		r.length++
+	}
 	r.Color = Black
-	r.length++
 }
 
 /*
 设置节点颜色并改变黑色节点的总长度
 */
 func (r *RedBlackNode) setRedColor() {
+	if r.Color == Black {
+		r.length--
+	}
 	r.Color = Red
-	r.length--
 }
 
 /*
 检测红黑树是否平衡
 */
 func (r *RedBlackNode) check() *RedBlackNode {
+	if r.balance() {
+		return r
+	}
 	return r.change()
 }
 
 /*
-调整红黑树
+调整红黑树,规则1交给tree处理
 */
 func (r *RedBlackNode) change() *RedBlackNode {
-	if r.Left == nil || r.Right == nil {
-		if r.Color == Red {
-			r.setBlackColor()
-		}
-		//黑色节点直接就平衡，因为新插入的子节点肯定是红色
+	//当前节点为红色则交由父节点处理
+	if r.Color == Red {
 		return r
 	}
-	if !r.checkBalance() {
-		return r.changeNode()
+	if r.isRule2() {
+		return r
 	}
+	if r.isRule3() {
+		return r.rule3()
+	}
+	if r.isRule4() {
+		return r.rule4()
+	}
+	if r.isRule5() {
+		return r.rule5()
+	}
+	return r
+}
+
+/*
+规则2
+*/
+func (r *RedBlackNode) isRule2() bool {
+	//能走到这里，肯定有一个节点不为空
+	if r.Left == nil {
+		if r.Right.Color == Red && r.Right.isLeaf() {
+			return true
+		}
+	}
+	if r.Right == nil {
+		if r.Left.Color == Red && r.Left.isLeaf() {
+			return true
+		}
+	}
+	return false
+}
+
+func (r *RedBlackNode) isRule3() bool {
+	if r.Left != nil && r.Right != nil {
+		//外部条件已经判断为这个节点为黑色，且不平衡，则左右孩子都为红色时则为规则3
+		if r.Left.Color == Red && r.Right.Color == Red {
+			return true
+		}
+	}
+	return false
+}
+
+/*
+规则4,节点的左孩子为红色，左孩子的右孩子为红色，右孩子不存在或右孩子为黑色。节点的右孩子为红色，右孩子的左孩子为红色，左孩子不存在或左孩子为黑色
+*/
+func (r *RedBlackNode) isRule4() bool {
+	if r.Left == nil || r.Left.Color == Black {
+		if r.Right.Left == nil {
+			return false
+		}
+		if r.Right.Color == Red && r.Right.Left.Color == Red {
+			return true
+		}
+		return false
+	}
+	if r.Right == nil || r.Right.Color == Black {
+		if r.Left.Right == nil {
+			return false
+		}
+		if r.Left.Color == Red && r.Left.Right.Color == Red {
+			return true
+		}
+	}
+	return false
+}
+
+/*
+是否是规则5
+*/
+func (r *RedBlackNode) isRule5() bool {
+	if r.Right == nil || r.Right.Color == Black {
+		if r.Left.Color == Black || r.Left.Left == nil {
+			return false
+		}
+		if r.Left.Color == Red && r.Left.Left.Color == Red {
+			return true
+		}
+	}
+	return false
+}
+
+func (r *RedBlackNode) rule3() *RedBlackNode {
+	r.setRedColor()
+	r.Left.setBlackColor()
+	r.Right.setRedColor()
+	return r
+}
+
+/*
+规则4
+*/
+func (r *RedBlackNode) rule4() *RedBlackNode {
+	if r.Left != nil && r.Left.Color == Red {
+		return r.lr()
+	}
+	return r.rl()
+}
+
+/*
+规则5
+*/
+func (r *RedBlackNode) rule5() *RedBlackNode {
+	if r.Left.Color == Red {
+		return r.ll()
+	}
+	return r.rr()
+}
+
+/*
+rl双旋转
+*/
+func (r *RedBlackNode) rl() *RedBlackNode {
+	right := r.Right
+	r.Right = right.Left
+	right.Left = r.Right.Right
+	r.Right.Right = right
+	return r.rr()
+}
+
+/*
+rr单旋转
+*/
+func (r *RedBlackNode) rr() *RedBlackNode {
+	right := r.Right
+	r.Right = right.Right
+	right.Right = r.Right.Left
+	r.Right.Left = right
+	return r
+}
+
+/*
+lr双旋转
+*/
+func (r *RedBlackNode) lr() *RedBlackNode {
+	left := r.Left
+	r.Left = left.Right
+	left.Right = r.Left.Left
+	r.Left.Left = left
+	return r.ll()
+}
+
+/*
+ll单旋转
+*/
+func (r *RedBlackNode) ll() *RedBlackNode {
+	left := r.Left
+	r.Left = left.Left
+	left.Left = r.Left.Right
+	r.Left.Right = left
 	return r
 }
 
 /*
 检测是否平衡
 */
-func (r *RedBlackNode) checkBalance() bool {
+func (r *RedBlackNode) balance() bool {
 	if r.Color == Red && (r.Left.Color == Red || r.Right.Color == Red) {
 		return false
 	}
@@ -140,4 +281,11 @@ func (r *RedBlackNode) changeRedBranch() *RedBlackNode {
 func (r *RedBlackNode) changeBranch() *RedBlackNode {
 	r.setBlackColor()
 	return r
+}
+
+func (r *RedBlackNode) isLeaf() bool {
+	if r.Left == nil && r.Right == nil {
+		return true
+	}
+	return false
 }
