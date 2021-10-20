86e5d4933f5fee03a00bfdf22ba8887b6b571aa8
lihao <568330385@qq.com>
2021-10-20 15:38:45 +0800

    增加查找数组中的重复数字

---
+++ algorithm/leetcode/string/数组中重复的数字_test.go
@@ -0,0 +1,32 @@
+package string
+
+import "testing"
+
+func findRepeatNumber(nums []int) int {
+	m := make([]int, 100001)
+	for _, v := range nums {
+		a := m[v]
+		if a > 0 {
+			return v
+		}
+		m[v]++
+	}
+	return -1
+}
+
+func TestFindRepeatNumber(t *testing.T) {
+	arr := []int{3, 4, 2, 0, 0, 1}
+	n := findRepeatNumber(arr)
+	if n != 2 && n != 3 {
+		t.Fatal("测试失败")
+		return
+	}
+	t.Log("测试通过")
+}
+
+func TestArr(t *testing.T) {
+	arr := make([]int, 10)
+	arr[0]++
+	arr[1]++
+	t.Log(arr)
+}
