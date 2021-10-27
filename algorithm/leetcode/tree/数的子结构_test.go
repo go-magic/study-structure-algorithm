d8a082b17b4b3eaa5bc3ccbb70314b5a409cb00c
lihao <568330385@qq.com>
2021-10-27 15:19:43 +0800

    增加树的子结构

--- algorithm/leetcode/tree/数的子结构_test.go
+++ algorithm/leetcode/tree/数的子结构_test.go
@@ -9,38 +9,62 @@ func TestiIsSubStructure(t *testing.T) {
 	t.Log("测试通过", trees)
 }
 
+var ret = make([]*TreeNode, 0)
+
 func isSubStructure(A *TreeNode, B *TreeNode) bool {
+	if A == nil || B == nil {
+		return false
+	}
+	ret = make([]*TreeNode, 0)
+	findNode(B.Val, A)
+	for _, node := range ret {
+		if ok := exist(node, B); ok {
+			return true
+		}
+	}
 	return false
 }
 
-func findNode(val int, node *TreeNode) *TreeNode {
-	if node == nil {
-		return nil
+func exist(A *TreeNode, B *TreeNode) bool {
+	if B == nil {
+		return true
 	}
-	if node.Val == val {
-		return node
+	if A == nil {
+		return false
 	}
-	var n *TreeNode
-	if node.Left != nil {
-		n = findNode(val, node.Left)
+	if A.Val != B.Val {
+		return false
 	}
-	if n != nil {
-		return n
+	if ok := exist(A.Left, B.Left); !ok {
+		return false
 	}
-	if node.Right != nil {
-		n = findNode(val, node.Right)
+	return exist(A.Right, B.Right)
+}
+
+func findNode(val int, A *TreeNode) {
+	if A == nil {
+		return
 	}
-	return n
+	if A.Val == val {
+		ret = append(ret, A)
+	}
+	findNode(val, A.Left)
+	findNode(val, A.Right)
 }
 
 func TestFindNode(t *testing.T) {
-	n := NewTreeNode(10)
-	n.Left = NewTreeNode(8)
-	n.Right = NewTreeNode(14)
+	n := NewTreeNode(3)
+	n.Left = NewTreeNode(4)
+	n.Right = NewTreeNode(5)
 	n.Left.Left = NewTreeNode(4)
-	n.Left.Right = NewTreeNode(9)
-	n.Right.Left = NewTreeNode(18)
-	n.Right.Right = NewTreeNode(24)
-	r := findNode(14, n)
-	t.Log(r)
+	n.Left.Right = NewTreeNode(2)
+	n.Left.Left.Left = NewTreeNode(1)
+
+	b := NewTreeNode(4)
+	b.Left = NewTreeNode(1)
+	if ok := isSubStructure(n, b); !ok {
+		t.Fatal("测试不通过")
+		return
+	}
+	t.Log("测试通过")
 }
